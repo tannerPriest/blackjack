ca5f6c76759758b5f61c2398f22a9f09d68e6bd1
killerwhiteshark <killerwhiteshark@users.noreply.github.com>
2019-12-17 09:45:24 -0800

    JS refactoring

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
 # blackjack
-General Assembly Project 1 - BlackJack
+ ### General Assembly Project 1 - BlackJack
+
+- A web browser based game of the card game Black Jack built from HTML, CSS, and Javascript.
+
+---- 
+## WireFrames
+
+- Bootstrap v4
+
+---
+
+## Game Logic
+---
+
+- Composed of 4 buttons, all of the event listeners are at below of the const and let variables in the JS file. 
+- All DOM selectors are in const variables.
+
+- The Deck of cards are sorted each card by objects with a key of total value King = "value: 10" and image key with a value path of each card.
+ The all card objects are all sorted in the deck array. 
+ - A const of deckArray is assigned to shuffle Deck when page loads, Render() is keeping track of how many cards are in the shuffled array. When Deck array becomes less than 25 cards, the shuffle(), will empty the replace the Deck with original deck and reshuffle that. 
+
+- Balance has a start of 100 credits and user is reduced to only allowed to bet either 5 or 10 credits a hand. 
+-Balance does display message and usermasked to reset game if balance is less that bet amount.
+
+- Cards are delt out one at a time. Player is checked for 21. If Black Jack or 21 player auto wins and is awarded the bet amount accordingly. 
+
+- Hit/Stay buttons are disabled when waiting for bet, and enabled when bet is made. Bet buttons are then disabled and waits for next deal. 
+
+- Computer Dealer is built to hit if dealer hand is below 17. 
+
+- Aces are recgonized and auto changed if it is in the players hands insterest to staying below 21. 
--- js/main.js
+++ js/main.js
@@ -214,7 +214,7 @@ let deck = [
     }
 ];
 
-let deckArray = deck.sort(() => Math.random() - 0.5)
+let deckArray = deck.sort(() => Math.random() - 0.5);
 const playOne = document.getElementById('play-1');
 const playTwo = document.getElementById('play-2');
 const dealerOne = document.getElementById('deal-1');
@@ -228,6 +228,7 @@ const playCounter = document.getElementById('play-count');
 const playMessage = document.getElementById('play-mess');
 const dealerMessage = document.getElementById('deal-mess');
 const cards = document.getElementsByTagName('img');
+const buttons = document.getElementsByTagName('button');
 
 let currentBet = '';
 let balance    = 100;
@@ -244,27 +245,7 @@ hitB.disabled = true;
 betFive.disabled = false;
 betTen.disabled = false;
 
-// const init = () => {
-//     currentBet = '';
-//     balance = 100;
-//     stayB.disabled = true;
-//     hitB.disabled = true;
-//     betFive.disabled = false;
-//     betTen.disabled = false;
-//     playerHand = [];
-//     dealerHand = [];
-//     playerTotal = [];
-//     dealerTotal = [];
-//     playOne.src = '/playingCards/backImage.webp';
-//     playTwo.src = '/playingCards/backImage.webp';
-//     dealerOne.src = '/playingCards/backImage.webp';
-//     dealerTwo.src = '/playingCards/backImage.webp';
-//     playMessage.textContent = 'MAKE YOUR BET!'
-//     playerAdd = playerTotal.reduce((a, b) => a + b, 0);
-//     dealerAdd = dealerTotal.reduce((a, b) => a + b, 0);
-//     bet = false;
-// }
-
+//Make const betFive and betTen and pass in for checkBalance;
 betFive.addEventListener("click", function(){
     currentBet = 5;
     checkBalance(currentBet);
@@ -273,9 +254,32 @@ betTen.addEventListener("click", function(){
     currentBet = 10;
     checkBalance(currentBet);
 });
+hitB.addEventListener('click', hitHandler);
+stayB.addEventListener('click', stayHandler);
+
+function hitHandler(){
+    var x = deckArray.pop()
+    playTwo.src = x.image;
+    if(x.value === 11 && playerAdd < 11){
+        x.value = 1;
+    }
+    playerHand.push(x);
+    playerTotal.push(x.value);
+    playerAdd = playerTotal.reduce((a, b) => a + b, 0);
+    playerTurn();
+};
+function stayHandler(){
+    stayB.disabled = true;
+    hitB.disabled = true;
+    betFive.disabled = true;
+    betTen.disabled = true;
+    dealerTwo.src = dealerHand[1].image;
+    dealerTurn();
+    render();
+};
 function checkBalance(betAmount){
-    if(betAmount > balance){
-        playMessage.textContent = 'Sorry Insufficient Funds, Would you like to reset?';
+    if(balance < 5){
+        playMessage.textContent = 'SORRY INSUFFICIENT FUNDS, GAME OVER!';
         return;
     } else{
         balance -= currentBet;
@@ -284,13 +288,23 @@ function checkBalance(betAmount){
     }
 }
 function checkForTwentyOne(){
-    for(let i = 0; i < 2; i++){
+
+    for(let i = 0; i < 4; i++){
         var x = deckArray.pop()
-        playerHand.push(x);
-        playerTotal.push(x.value);
-  }
+        if(i % 2 === 0){
+            playerHand.push(x);
+            playerTotal.push(x.value);
+        } else {
+            dealerHand.push(x);
+            dealerAdd.push(x.value);
+        };
+  };
   playerAdd = playerTotal.reduce((a, b) => a + b, 0);
-    if(playerAdd === 21){
+    if(playerAdd === 22){
+        playerHand[1].value = 1;
+        inBet();
+        deal();
+    }else if(playerAdd === 21){
         blackJack();
         return;
     }else{
@@ -302,18 +316,13 @@ function checkForTwentyOne(){
 function blackJack(){
     playOne.src = playerHand[0].image;
     playTwo.src = playerHand[playerHand.length - 1].image;
-    playMessage.textContent = "BlackJack!"
+    playMessage.textContent = "21!!! YOU WIN!"
     playCounter.textContent = playerAdd;
     balance += currentBet * 2;
     window.setTimeout(waitForbet, 3000)
     render();
 }
 function deal(){
-  for(let i = 0; i < 2; i++){
-    let x = deckArray.pop();
-    dealerHand.push(x);
-    dealerTotal.push(x.value);
-};
     playMessage.textContent = 'YOUR MOVE.'
     showCards();
 }
@@ -324,36 +333,12 @@ function showCards(){
     dealerAdd = dealerHand[0].value;
     render();
 }
-function compareHands(){
-    playerAdd = playerTotal.reduce((a, b) => a + b, 0);
-    dealerAdd = dealerTotal.reduce((a, b) => a + b, 0);
-    let lastDeal = dealerHand[dealerHand.length-1].image;
-    dealerTwo.src = lastDeal;
-    stayB.disabled = true;
-    hitB.disabled = true;
-
-    if(dealerAdd > 21){
-        balance += currentBet * 2;
-        render();
-        playMessage.textContent = 'YOU WIN THIS HAND! DEALER BUST!';
-        console.log(dealerHand)
-        window.setTimeout(waitForbet, 3000)
-    } else if(playerAdd > dealerAdd){
-        balance += currentBet * 2.5;
-        playMessage.textContent = 'YOU WIN THIS HAND!';
-        render();
-        window.setTimeout(waitForbet, 3000)
-    } else if(dealerAdd > playerAdd){
-        playMessage.textContent = 'DEALER WINS THIS HAND!';
-        render();
-        window.setTimeout(waitForbet, 3000)
-    }
-}
 function inBet(){
     betFive.disabled = true;
     betTen.disabled = true;
     stayB.disabled = false;
     hitB.disabled = false;
+    bet = true;
 }
 function waitForbet(){
     stayB.disabled = true;
@@ -380,8 +365,8 @@ function playerTurn(){
         return;
     } else if(playerAdd > 21){
         for(let i = 0; i < playerHand.length; i++){
-            if(i.value === 11){
-                i.value = 1;
+            if(playerHand[i].value === 11){
+                playerHand[i].value = 1;
             }
             for(let i = 0; i < playerTotal.length; i++){
                 if(i === 11){
@@ -421,25 +406,262 @@ function dealerTurn () {
         } else {
             compareHands();
         }
+};
+function compareHands(){
+    playerAdd = playerTotal.reduce((a, b) => a + b, 0);
+    dealerAdd = dealerTotal.reduce((a, b) => a + b, 0);
+    let lastDeal = dealerHand[dealerHand.length-1].image;
+    dealerTwo.src = lastDeal;
+    stayB.disabled = true;
+    hitB.disabled = true;
+    if(playerAdd === dealerAdd){
+        balance += currentBet;
+        playMessage.textContent = 'PUSH!'
+        window.setTimeout(waitForbet, 3000)
+    }
+    if(dealerAdd > 21){
+        balance += currentBet * 2;
+        render();
+        playMessage.textContent = 'YOU WIN THIS HAND! DEALER BUST!';
+        window.setTimeout(waitForbet, 3000)
+    } else if(playerAdd > dealerAdd){
+        balance += currentBet * 2.5;
+        playMessage.textContent = 'YOU WIN THIS HAND!';
+        render();
+        window.setTimeout(waitForbet, 3000)
+    } else if(dealerAdd > playerAdd){
+        playMessage.textContent = 'DEALER WINS THIS HAND!';
+        render();
+        window.setTimeout(waitForbet, 3000)
+    }
 }
+function shuffle (){
+    buttons.disabled = true;
+    playMessage.textContent = 'RESHUFFLING DECK.'
+    deck = [
+        {
+            value: 2,
+            image: 'playingCards/2C.jpg'
+        },
+        {
+            value: 2,
+            image: 'playingCards/2D.jpg'
+        },
+        {
+            value: 2,
+            image: 'playingCards/2H.jpg'
+        },
+        {
+            value: 2,
+            image: 'playingCards/2S.jpg'
+        },
+        {
+            value: 3,
+            image: 'playingCards/3C.jpg'
+        },
+        {
+            value: 3,
+            image: 'playingCards/3D.jpg'
+        },
+        {
+            value: 3,
+            image: 'playingCards/3H.jpg'
+        },
+        {
+            value: 3,
+            image: 'playingCards/3S.jpg'
+        },
+        {
+            value: 4,
+            image: 'playingCards/4C.jpg'
+        },
+        {
+            value: 4,
+            image: 'playingCards/4D.jpg'
+        },
+        {
+            value: 4,
+            image: 'playingCards/4H.jpg'
+        },
+        {
+            value: 4,
+            image: 'playingCards/4S.jpg'
+        },
+        {
+            value: 5,
+            image: 'playingCards/5C.jpg'
+        },
+        {
+            value: 5,
+            image: 'playingCards/5D.jpg'
+        },
+        {
+            value: 5,
+            image: 'playingCards/5H.jpg'
+        },
+        {
+            value: 5,
+            image: 'playingCards/5S.jpg'
+        },
+        {
+            value: 6,
+            image: 'playingCards/6C.jpg'
+        },
+        {
+            value: 6,
+            image: 'playingCards/6D.jpg'
+        },
+        {
+            value: 6,
+            image: 'playingCards/6H.jpg'
+        },
+        {
+            value: 6,
+            image: 'playingCards/6S.jpg'
+        },
+        {
+            value: 7,
+            image: 'playingCards/7C.jpg'
+        },
+        {
+            value: 7,
+            image: 'playingCards/7D.jpg'
+        },
+        {
+            value: 7,
+            image: 'playingCards/7H.jpg'
+        },
+        {
+            value: 7,
+            image: 'playingCards/7S.jpg'
+        },
+        {
+            value: 8,
+            image: 'playingCards/8C.jpg'
+        },
+        {
+            value: 8,
+            image: 'playingCards/8D.jpg'
+        },
+        {
+            value: 8,
+            image: 'playingCards/8H.jpg'
+        },
+        {
+            value: 8,
+            image: 'playingCards/8S.jpg'
+        },
+        {
+            value: 9,
+            image: 'playingCards/9C.jpg'
+        },
+        {
+            value: 9,
+            image: 'playingCards/9D.jpg'
+        },
+        {
+            value: 9,
+            image: 'playingCards/9H.jpg'
+        },
+        {
+            value: 9,
+            image: 'playingCards/9S.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/10C.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/10D.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/10H.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/10C.jpg'
+        },
+        {
+            value: 11,
+            ace: true,
+            image: 'playingCards/AC.jpg'
+        },
+        {
+            value: 11,
+            ace: true,
+            image: 'playingCards/AD.jpg'
+        },
+        {
+            value: 11,
+            ace: true,
+            image: 'playingCards/AH.jpg'
+        },
+        {
+            value: 11,
+            ace: true,
+            image: 'playingCards/AS.jpg'
+        },
+        {
+            value: 11,
+            image: 'playingCards/JC.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/JD.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/JH.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/JS.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/KC.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/KD.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/KH.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/KS.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/QC.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/QD.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/QH.jpg'
+        },
+        {
+            value: 10,
+            image: 'playingCards/QS.jpg'
+        }
+    ];
+    window.setTimeout(waitForbet, 2000);
+};
 function render(){
     playerAdd = playerTotal.reduce((a, b) => a + b, 0);
     dealCounter.innerHTML = dealerAdd;
     playCounter.innerHTML = playerAdd;
     document.querySelector('#bet-amount').textContent = `$${currentBet}`;
     document.querySelector('#balance').textContent = `$${balance}`;
-}
-hitB.addEventListener('click', function(){
-    var x = deckArray.pop()
-    playTwo.src = x.image;
-    playerHand.push(x);
-    playerTotal.push(x.value);
-    playerAdd = playerTotal.reduce((a, b) => a + b, 0);
-    playerTurn();
-});
-stayB.addEventListener('click', function(){
-    dealerTwo.src = dealerHand[1].image;
-    render();
-    dealerTurn();
-});
+    if(deck.length < 25 && bet === false){
+        shuffle();
+    }
+};
 
